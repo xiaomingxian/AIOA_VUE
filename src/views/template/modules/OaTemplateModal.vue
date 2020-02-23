453ff198dabac5788393bca7dc78ab04601bbe1a
chenwenkai88 <773174119@qq.com>
2020-02-23 14:53:56 +0800

    模板管理-编辑文档前端验证完善

--- src/views/template/modules/OaTemplateModal.vue
+++ src/views/template/modules/OaTemplateModal.vue
@@ -14,7 +14,7 @@
           :labelCol="labelCol"
           :wrapperCol="wrapperCol"
           label="模版名称">
-          <a-input placeholder="" v-decorator="['sname', {}]" />
+          <a-input placeholder="" v-decorator="['sname', {}]"/>
         </a-form-item>
         <a-form-item
           :labelCol="labelCol"
@@ -22,13 +22,14 @@
           label="稿纸头规则">
           <!--<a-input-number v-decorator="[ 'ititleRuleId', {}]" />-->
           <!--<a-select defaultValue="selected ${value}" v-decorator="[ 'ititleRuleId', {}]">-->
-            <!--<a-select-option value="0">稿纸头列表</a-select-option>-->
-            <!--</a-select-option>-->
+          <!--<a-select-option value="0">稿纸头列表</a-select-option>-->
+          <!--</a-select-option>-->
           <!--</a-select>-->
 
           <a-select
             v-decorator="[ 'ititleRuleId', {}]">
-            <a-select-option  v-for="(item,index) in selectData" :key="index" :value="item.iid">{{item.stitleName}}</a-select-option>
+            <a-select-option v-for="(item,index) in selectData" :key="index" :value="item.iid">{{item.stitleName}}
+            </a-select-option>
 
           </a-select>
         </a-form-item>
@@ -38,7 +39,7 @@
           :wrapperCol="wrapperCol"
           label="模版类型">
           <!--<a-input placeholder="" v-decorator="['itype', {}]" />-->
-          <a-radio-group v-model="fileType" v-decorator="['itype', {}]" >
+          <a-radio-group v-model="fileType" v-decorator="['itype', {}]">
             <a-radio :value="1">上报</a-radio>
             <a-radio :value="2">下发</a-radio>
             <a-radio :value="3">办文单</a-radio>
@@ -48,14 +49,18 @@
           :labelCol="labelCol"
           :wrapperCol="wrapperCol"
           label="备注">
-          <a-input placeholder="" v-decorator="['sremarks', {}]" />
+          <a-input placeholder="" v-decorator="['sremarks', {}]"/>
         </a-form-item>
         <a-form-item
           :labelCol="labelCol"
           :wrapperCol="wrapperCol"
           label="附件上传">
-          <a-upload name="file" :fileList="fileList" :beforeUpload="beforeUpload" :showUploadList="false" :multiple="false" :headers="headers" @change="handleChange">
-            <a-button :disabled="uploading"><a-icon type="upload" />点击上传</a-button>
+          <a-upload name="file" :fileList="fileList" :beforeUpload="beforeUpload" :showUploadList="false"
+                    :multiple="false" :headers="headers" @change="handleChange">
+            <a-button :disabled="uploading">
+              <a-icon type="upload"/>
+              点击上传
+            </a-button>
           </a-upload>
         </a-form-item>
 
@@ -65,8 +70,8 @@
           :wrapperCol="wrapperCol"
           label="附件名称">
           <a-input v-model="upFileName"/>
-          <a-button type="primary" icon="plus" @click="deleteFileName">删除</a-button>
-          <a-input v-show="false"  v-model="fileId" v-decorator="['ifileId', {}]"/>
+          <a-button type="primary" @click="deleteFileName">删除</a-button>
+          <a-input v-show="false" v-model="fileId" v-decorator="['ifileId', {}]"/>
         </a-form-item>
 
         <a-form-item
@@ -76,12 +81,14 @@
           label="附件名称">
           <div class="qiCao"><span class="hoverred" @click="qiCao1(9,fileData)" style="font-size: 16px;">{{fileData.sfileName}}</span>
             <span class="delCss">
-                <img :title="fileBtnName(1)" v-if="isShow !==isSuffex(fileData.sfileName)" class="pices" @click.stop="qiCao2(10,fileData)" src="../../../../src/assets/set.png"/>
-<!--                <img :title="fileBtnName(2)" class="pices" @click.stop="updateFileName(fileData)" src="../../../../src/assets/setName.png"/>-->
-                <img :title="fileBtnName(3)" class="pices" @click.stop="deleteFileName()" src="../../../../src/assets/delete.png"/>
+                <img :title="fileBtnName(1)" v-if="isShow !==isSuffex(fileData.sfileName)" class="pices"
+                     @click.stop="qiCao2(10,fileData)" src="../../../../src/assets/set.png"/>
+              <!--                <img :title="fileBtnName(2)" class="pices" @click.stop="updateFileName(fileData)" src="../../../../src/assets/setName.png"/>-->
+                <img :title="fileBtnName(3)" class="pices" @click.stop="deleteFileName()"
+                     src="../../../../src/assets/delete.png"/>
               </span>
           </div>
-          <a-input v-show="false"  v-model="fileId" v-decorator="['ifileId', {}]"/>
+          <a-input v-show="false" v-model="fileId" v-decorator="['ifileId', {}]"/>
         </a-form-item>
 
       </a-form>
@@ -90,115 +97,120 @@
 </template>
 
 <script>
-  import { httpAction } from '@/api/manage'
+  import {httpAction} from '@/api/manage'
   import pick from 'lodash.pick'
-  import { getAction } from '@/api/manage'
+  import {getAction} from '@/api/manage'
   import Vue from 'vue'
   import {ACCESS_TOKEN} from '@/store/mutation-types'
   import AFormItem from "ant-design-vue/es/form/FormItem";
   import {busdataTemplate} from "@/views/buttons/btn-js/busdataTemplate";
   import DelTime from "../../buttons/DelTimeModal";
+
   export default {
     name: "OaTemplateModal",
     components: {AFormItem},
     mixins: [busdataTemplate],
-    data () {
+    data() {
       return {
-        fileData:{
-          iid:'',
-          sfileType:'',
-          sfileName:'',
-          sfilePath:''
+        fileData: {
+          iid: '',
+          sfileType: '',
+          sfileName: '',
+          sfilePath: ''
         },
-        fileType:"",
-        upFileName:'',
+        fileType: "",
+        upFileName: '',
         upFailId: 0,
-        fileId:'',
+        fileId: '',
         fileList: [],
         uploading: false,
-        isShow:false,
+        isShow: false,
         headers: {
           'X-Access-Token': Vue.ls.get(ACCESS_TOKEN)
         },
-        title:"操作",
+        title: "操作",
         visible: false,
         model: {},
         labelCol: {
-          xs: { span: 24 },
-          sm: { span: 5 },
+          xs: {span: 24},
+          sm: {span: 5},
         },
         wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 16 },
+          xs: {span: 24},
+          sm: {span: 16},
         },
-        selectData:[],
+        selectData: [],
         confirmLoading: false,
         form: this.$form.createForm(this),
-        validatorRules:{
-        // iId:{rules: [{ required: true, message: '请输入主键id!' }]},
+        validatorRules: {
+          // iId:{rules: [{ required: true, message: '请输入主键id!' }]},
         },
         url: {
           add: "/papertitle/oaTemplate/add",
           edit: "/papertitle/oaTemplate/edit",
-          upload:'/papertitle/oaTemplate/upload'
+          upload: '/papertitle/oaTemplate/upload'
         },
       }
     },
-    created(){
+    created() {
     },
     methods: {
 
-      add () {
+      add() {
         this.getSelection();
         this.upFailId = 0;
         this.upFileName = '';
-        this.fileUpload = window._CONFIG['domianURL'] +'/papertitle/oaTemplate/upload';
+        // this.fileUpload = window._CONFIG['domianURL'] + '/papertitle/oaTemplate/upload';
         this.form.resetFields();
         this.model = Object.assign({}, {});
         this.visible = true;
         this.$nextTick(() => {
-          this.form.setFieldsValue(pick(this.model,'iid','itype','sname','ifileId','ititleRuleId','sremarks'))
+          this.form.setFieldsValue(pick(this.model, 'iid', 'itype', 'sname', 'ifileId', 'ititleRuleId', 'sremarks'))
         });
 
       },
-      edit (record) {
+      edit(record) {
         this.getSelection();
-        if (record.ifileId){
-          this.upFailId = 1;
+        if (record.ifileId) {
           this.getFileNameById(record.ifileId);
-          this.uploading = true;
         } else {
           this.upFailId = 0;
           this.uploading = false;
         }
         this.fileType = record.itype;
-        this.fileUpload = window._CONFIG['domianURL'] +'/papertitle/oaTemplate/upload';
+        // this.fileUpload = window._CONFIG['domianURL'] + '/papertitle/oaTemplate/upload';
         this.form.resetFields();
         this.model = Object.assign({}, record);
         this.visible = true;
         this.$nextTick(() => {
-          this.form.setFieldsValue(pick(this.model,'iid','itype','sname','ifileId','ititleRuleId','sremarks'))
+          this.form.setFieldsValue(pick(this.model, 'iid', 'itype', 'sname', 'ifileId', 'ititleRuleId', 'sremarks'))
         });
       },
-      getFileNameById(id){
+      getFileNameById(id) {
         let url = "/papertitle/oaTemplate/getFileNameById";
-        getAction(url,{ifileId:id}).then((res)=>{
-          this.upFileName = res.result.sfileName;
-          this.fileData.iid = res.result.iid;
-          this.fileData.sfileName = res.result.sfileName;
-          this.fileData.sfilePath = res.result.sfilePath;
-          this.fileData.sfileType = res.result.sfileType;
+        getAction(url, {ifileId: id}).then((res) => {
+          if (res.success && res.result !== null) {
+            this.upFailId = 1;
+            this.uploading = true;
+            this.fileData.iid = res.result.iid;
+            this.fileData.sfileName = res.result.sfileName;
+            this.fileData.sfilePath = res.result.sfilePath;
+            this.fileData.sfileType = res.result.sfileType;
+          } else {
+            this.$message.error("模板不存在！")
+          }
+
         })
       },
-      beforeUpload:function(file,fileList){
+      beforeUpload: function (file, fileList) {
         this.upFailId = 1;
         this.upFileName = file.name;
         this.uploading = true;
         this.fileList = [...this.fileList, file];
         return false;
       },
-      deleteFileName(){
-        let  that = this;
+      deleteFileName() {
+        let that = this;
         that.upFailId = 0;
         that.upFileName = '';
         this.model.ifileId = "";
@@ -208,20 +220,19 @@
         });
         this.uploading = false;
       },
-      close () {
+      close() {
         this.upFailId = 0;
         this.uploading = false;
         this.fileType = "";
         this.fileData.iid = '';
-        this.fileData.sfileType ='';
+        this.fileData.sfileType = '';
         this.fileData.sfilePath = '';
         this.fileData.sfileName = '';
         this.$emit('close');
         this.visible = false;
       },
-
       handleUpload() {
-        const { fileList } = this;
+        const {fileList} = this;
         const data = new FormData();
         fileList.forEach(file => {
           data.append('file', file);
@@ -229,69 +240,69 @@
         this.uploading = true;
         let url = this.url.upload;
         let method = 'post';
-        if (this.fileType == 1){
+        if (this.fileType == 1) {
           this.fileType = 2;
         }
-        data.append("fileType",this.fileType);
-        httpAction(url,data,method).then((res)=>{
+        data.append("fileType", this.fileType);
+        httpAction(url, data, method).then(res => {
           this.fileList = [];
           this.uploading = false;
           this.model.ifileId = res.result.iid;
-          this.$nextTick(() => {
-            this.form.setFieldsValue(pick(this.model, 'ifileId'))
-          });
+          this.form.setFieldsValue(pick(this.model, 'ifileId'))
+          this.setFormData();
         });
       },
-      handleOk () {
+      setFormData() {
+        // 触发表单验证
         const that = this;
-        that.handleUpload();
-       setTimeout(()=>{
-         // 触发表单验证
-         this.form.validateFields((err, values) => {
-           if (!err) {
-             that.confirmLoading = true;
-             let httpurl = '';
-             let method = '';
-             if(!this.model.iid){
-               httpurl+=this.url.add;
-               method = 'post';
-             }else{
-               httpurl+=this.url.edit;
-               method = 'put';
-             }
-
-             let formData = Object.assign(this.model, values);
-             console.log( formData);
-             httpAction(httpurl,formData,method).then((res)=>{
-               if(res.success){
-                 that.$message.success(res.message);
-                 that.$emit('ok');
-               }else{
-                 that.$message.warning(res.message);
-               }
-             }).finally(() => {
-               that.confirmLoading = false;
-               that.close();
-             })
-
-
-
-           }
-         })
-       },800)
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if (!this.model.iid) {
+              httpurl += this.url.add;
+              method = 'post';
+            } else {
+              httpurl += this.url.edit;
+              method = 'put';
+            }
+            let formData = Object.assign(this.model, values);
+            // alert(JSON.stringify(formData))
+            // console.log(formData);
+            httpAction(httpurl, formData, method).then((res) => {
+              if (res.success) {
+                that.$message.success(res.message);
+                that.$emit('ok');
+              } else {
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+              that.close();
+            })
+          }
+        })
       },
-      handleCancel () {
+      handleOk() {
+        if (this.fileData.sfilePath === '') {
+          this.handleUpload();
+        } else {
+          this.setFormData();
+        }
+      },
+      handleCancel() {
         this.close()
       },
-      getSelection(){
+      getSelection() {
         let url = "/papertitle/paperTitleSetting/paperTitleList";
-        getAction(url).then((res)=>{
+        getAction(url).then((res) => {
           this.selectData = res.result;
         })
       },
-      handleChange(info){
-        if (info.file.status !== 'uploading'){
-          console.log(info.file,info.fileList);
+      handleChange(info) {
+        if (info.file.status !== 'uploading') {
+          console.log(info.file, info.fileList);
         }
         if (info.file.status === 'done') {
           if (info.file.response.success) {
@@ -302,10 +313,10 @@
               this.form.setFieldsValue(pick(this.model, 'ifileId'))
             });
             this.$message.success(`${info.file.name} 文件上传成功`);
-          }else {
+          } else {
             this.$message.error(`${info.file.name} ${info.file.response.message}.`);
           }
-        }else if (info.file.status ==='error'){
+        } else if (info.file.status === 'error') {
           this.$message.error(`文件上传失败: ${info.file.msg} `);
         }
 
@@ -329,7 +340,7 @@
     display: inline-block;
     padding-top: 1%;
     white-space: nowrap;
-    text-overflow:ellipsis;
+    text-overflow: ellipsis;
     overflow: hidden;
     float: left;
   }
