851dedf1d914d8fa4fce3d9d38ddf11ee8118a3e
陈鹏菲 <mvc1006@163.com>
2020-03-06 11:03:02 +0800

    去除  切换页签刷新

--- src/App.vue
+++ src/App.vue
@@ -45,19 +45,19 @@
 
       //监听页签切换 监听当页面是活动页时  刷新页面
 
-      var _this = this;
-
-      document.addEventListener('visibilitychange', function () {
-        // document.title = document.hidden?'拜拜11111':' 回来啦2222'
-        if (document.hidden) {
-
-          console.log(_this.$route.path.endsWith('NewOneParts'));
-          if (_this.$route.path.endsWith('NewOneParts') || _this.$route.path.endsWith('analysis') || _this.$route.path.endsWith('daynnalysis')) {
-            _this.reload()
-
-          }
-        }
-      })
+      // var _this = this;
+      //
+      // document.addEventListener('visibilitychange', function () {
+      //   // document.title = document.hidden?'拜拜11111':' 回来啦2222'
+      //   if (document.hidden) {
+      //
+      //     console.log(_this.$route.path.endsWith('NewOneParts'));
+      //     if (_this.$route.path.endsWith('NewOneParts') || _this.$route.path.endsWith('analysis') || _this.$route.path.endsWith('daynnalysis')) {
+      //       _this.reload()
+      //
+      //     }
+      //   }
+      // })
 
 
 
