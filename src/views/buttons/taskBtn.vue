ee990965cace0aa604d2f49008e28e24efb8ec56
xxm-xiaoxin <934792834@qq.com>
2020-03-06 16:12:46 +0800

    按钮

--- src/views/buttons/taskBtn.vue
+++ src/views/buttons/taskBtn.vue
@@ -9,6 +9,8 @@
     <!--<a-button  @click="nextTaskQuery">下一任务</a-button>-->
     <!--<a-button  @click="addUserOrDepart">追加</a-button>-->
     <!--<a-button  @click="insideReading">内部传阅</a-button>-->
+    <!--<a-button  @click="refreshIndexClose">关闭刷新父页面</a-button>-->
+    <!--<a-button  @click="deleteBusData">删除</a-button>-->
 
     <!--<div class="optionTip" @click="btnBoxShow">
       <a href="#">
