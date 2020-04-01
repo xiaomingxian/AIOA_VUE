182b1619fed37b38073c4200bd9c40fc7bfc40f8
xxm-xiaoxin <934792834@qq.com>
2020-04-01 12:29:12 +0800

    查询条件取消

--- src/views/system/SysUserAgentList.vue
+++ src/views/system/SysUserAgentList.vue
@@ -2,66 +2,66 @@
   <a-card :bordered="false">
 
     <!-- 查询区域 -->
-    <div class="table-page-search-wrapper">
-      <a-form layout="inline">
-        <a-row :gutter="22">
+    <!--<div class="table-page-search-wrapper">-->
+      <!--<a-form layout="inline">-->
+        <!--<a-row :gutter="22">-->
 
-          <a-col :md="6" :sm="8">
-            <a-form-item label="用户名">
-              <a-input placeholder="请输入用户名" v-model="queryParam.userName"></a-input>
-            </a-form-item>
-          </a-col>
+          <!--<a-col :md="6" :sm="8">-->
+            <!--<a-form-item label="用户名">-->
+              <!--<a-input placeholder="请输入用户名" v-model="queryParam.userName"></a-input>-->
+            <!--</a-form-item>-->
+          <!--</a-col>-->
 
 
-          <a-col :md="6" :sm="8">
-            <a-form-item label="代理人">
-              <a-input placeholder="请输入代理人" v-model="queryParam.agentUserName"></a-input>
-            </a-form-item>
-          </a-col>
-          <a-col :md="6" :sm="6">
-            <a-form-item label="状态">
-              <!--<a-input placeholder="请输入状态0无效1有效" v-model="queryParam.status"></a-input>-->
-              <a-select v-model="queryParam.status" >
-                <a-select-option value="1">有效</a-select-option>
-                <a-select-option value="0">无效</a-select-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
           <!--<a-col :md="6" :sm="8">-->
             <!--<a-form-item label="代理人">-->
-              <!--<a-select  v-model="queryParam.agentUserName" placeholder="请选择代理人">-->
-                <!--<a-select-option v-for="(item,index) in postLists" :key="index" :value="item.agentUserName">{{item.agentUserName}}-->
-                <!--</a-select-option>-->
+              <!--<a-input placeholder="请输入代理人" v-model="queryParam.agentUserName"></a-input>-->
+            <!--</a-form-item>-->
+          <!--</a-col>-->
+          <!--<a-col :md="6" :sm="6">-->
+            <!--<a-form-item label="状态">-->
+              <!--&lt;!&ndash;<a-input placeholder="请输入状态0无效1有效" v-model="queryParam.status"></a-input>&ndash;&gt;-->
+              <!--<a-select v-model="queryParam.status" >-->
+                <!--<a-select-option value="1">有效</a-select-option>-->
+                <!--<a-select-option value="0">无效</a-select-option>-->
               <!--</a-select>-->
             <!--</a-form-item>-->
           <!--</a-col>-->
-          <template v-if="toggleSearchStatus">
-            <a-col :md="6" :sm="8">
-              <a-form-item label="开始时间">
-                <a-input placeholder="请输入开始时间" v-model="queryParam.startTime"></a-input>
-              </a-form-item>
-            </a-col>
-            <a-col :md="6" :sm="8">
-              <a-form-item label="结束时间">
-                <a-input placeholder="请输入结束时间" v-model="queryParam.endTime"></a-input>
-              </a-form-item>
-            </a-col>
+          <!--&lt;!&ndash;<a-col :md="6" :sm="8">&ndash;&gt;-->
+            <!--&lt;!&ndash;<a-form-item label="代理人">&ndash;&gt;-->
+              <!--&lt;!&ndash;<a-select  v-model="queryParam.agentUserName" placeholder="请选择代理人">&ndash;&gt;-->
+                <!--&lt;!&ndash;<a-select-option v-for="(item,index) in postLists" :key="index" :value="item.agentUserName">{{item.agentUserName}}&ndash;&gt;-->
+                <!--&lt;!&ndash;</a-select-option>&ndash;&gt;-->
+              <!--&lt;!&ndash;</a-select>&ndash;&gt;-->
+            <!--&lt;!&ndash;</a-form-item>&ndash;&gt;-->
+          <!--&lt;!&ndash;</a-col>&ndash;&gt;-->
+          <!--<template v-if="toggleSearchStatus">-->
+            <!--<a-col :md="6" :sm="8">-->
+              <!--<a-form-item label="开始时间">-->
+                <!--<a-input placeholder="请输入开始时间" v-model="queryParam.startTime"></a-input>-->
+              <!--</a-form-item>-->
+            <!--</a-col>-->
+            <!--<a-col :md="6" :sm="8">-->
+              <!--<a-form-item label="结束时间">-->
+                <!--<a-input placeholder="请输入结束时间" v-model="queryParam.endTime"></a-input>-->
+              <!--</a-form-item>-->
+            <!--</a-col>-->
 
-          </template>
-          <a-col :md="6" :sm="8">
-            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
-              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
-              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
-              <a @click="handleToggleSearch" style="margin-left: 8px">
-                {{ toggleSearchStatus ? '收起' : '展开' }}
-                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
-              </a>
-            </span>
-          </a-col>
+          <!--</template>-->
+          <!--<a-col :md="6" :sm="8">-->
+            <!--<span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">-->
+              <!--<a-button type="primary" @click="searchQuery" icon="search">查询</a-button>-->
+              <!--<a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>-->
+              <!--<a @click="handleToggleSearch" style="margin-left: 8px">-->
+                <!--{{ toggleSearchStatus ? '收起' : '展开' }}-->
+                <!--<a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>-->
+              <!--</a>-->
+            <!--</span>-->
+          <!--</a-col>-->
 
-        </a-row>
-      </a-form>
-    </div>
+        <!--</a-row>-->
+      <!--</a-form>-->
+    <!--</div>-->
 
     <!-- 操作按钮区域 -->
     <div class="table-operator">
@@ -71,26 +71,26 @@
                 <!--@change="handleImportExcel">-->
         <!--<a-button type="primary" icon="import">导入</a-button>-->
       <!--</a-upload>-->
-      <a-dropdown v-if="selectedRowKeys.length > 0">
-        <a-menu slot="overlay">
-          <a-menu-item key="1" @click="batchDel">
-            <a-icon type="delete"/>
-            删除
-          </a-menu-item>
-        </a-menu>
-        <a-button style="margin-left: 8px"> 批量操作
-          <a-icon type="down"/>
-        </a-button>
-      </a-dropdown>
+      <!--<a-dropdown v-if="selectedRowKeys.length > 0">-->
+        <!--<a-menu slot="overlay">-->
+          <!--<a-menu-item key="1" @click="batchDel">-->
+            <!--<a-icon type="delete"/>-->
+            <!--删除-->
+          <!--</a-menu-item>-->
+        <!--</a-menu>-->
+        <!--<a-button style="margin-left: 8px"> 批量操作-->
+          <!--<a-icon type="down"/>-->
+        <!--</a-button>-->
+      <!--</a-dropdown>-->
     </div>
 
     <!-- table区域-begin -->
     <div>
-      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
-        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{
-        selectedRowKeys.length }}</a>项
-        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
-      </div>
+      <!--<div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">-->
+        <!--<i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{-->
+        <!--selectedRowKeys.length }}</a>项-->
+        <!--<a style="margin-left: 24px" @click="onClearSelected">清空</a>-->
+      <!--</div>-->
 
       <a-table
         ref="table"
@@ -217,7 +217,7 @@
           }
         ],
         url: {
-          list: "/sys/sysUserAgent/list",
+          list: "/sys/sysUserAgent/myAgent",
           delete: "/sys/sysUserAgent/delete",
           deleteBatch: "/sys/sysUserAgent/deleteBatch",
           exportXlsUrl: "/sys/sysUserAgent/exportXls",
