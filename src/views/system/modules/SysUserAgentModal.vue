c89d6c0cca608ab76a109a1489ec7692e7facccd
xxm-xiaoxin <934792834@qq.com>
2020-04-20 14:26:24 +0800

    时间校验兼容ie

--- src/views/system/modules/SysUserAgentModal.vue
+++ src/views/system/modules/SysUserAgentModal.vue
@@ -123,7 +123,15 @@
           this.$message.error('结束时间不得为空')
           return true
         }
-        if (new Date(this.model.startTime) > new Date(this.model.endTime)) {
+
+        // var isIE = this.isIe() //判断是否IE<11浏览器
+
+        let start=this.model.startTime.replace(/-/g, '/')
+        let end=this.model.endTime.replace(/-/g, '/')
+
+        // alert(start+"    "+end)
+
+        if (new Date(start) > new Date(end)) {
           this.$message.error('结束时间不得小于开始时间')
           return true
         }
@@ -136,6 +144,36 @@
 
         return false
         // this.model.startTime
+      },
+      isIe(){
+
+        var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+        var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器
+        var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器
+        var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+        if(isIE) {
+          var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+          reIE.test(userAgent);
+          var fIEVersion = parseFloat(RegExp["$1"]);
+          if(fIEVersion == 7) {
+            return 7;
+          } else if(fIEVersion == 8) {
+            return 8;
+          } else if(fIEVersion == 9) {
+            return 9;
+          } else if(fIEVersion == 10) {
+            return 10;
+          } else {
+            return 6;//IE版本<=7
+          }
+        } else if(isEdge) {
+          return 'edge';//edge
+        } else if(isIE11) {
+          return 11; //IE11
+        }else{
+          return -1;//不是ie浏览器
+        }
+
       },
       edit(record) {
 
